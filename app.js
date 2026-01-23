7cf7a854644a0ff024d69b2e881fcae01622dc6d
thivagarradhakrishnan-create <thivagarradhakrishnan@sahayta.co.in>
2026-01-23 11:40:49 +0530

    Download image and doc

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://whatsapp-marketing-frontend-qzk7.vercel.app",
+      "https://whatsapp-marketing-frontend-three.vercel.app",
     ],
     credentials: true,
   })
